d37249f4a3eeeaa9bfb9a24135a8e68dec257f84
jinsijasi <jinsi.jasi16@gmail.com>
2021-11-18 08:17:15 +0100

    --- Auto-Git Commit ---

--- source_code/input_handler/src/input_handler_main.cpp
+++ source_code/input_handler/src/input_handler_main.cpp
@@ -3,8 +3,28 @@
 
 
 
+
+
 int main()
 {
+    CanTransmitter transmitter;
+    initscr();
+    cbreak();
+    noecho();
+    scrollok(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+    while(true)
+    {
+        char in;
+        while((in = getch()) == ERR){
+            if(in >= '0' && in <= '9')
+            {
+                transmitter.Set
+            }
+        }
+    }
+
+
 
 
    return 0; 
