3bce5c8918b4be5d6434d701a03b19a2f0c5d46a
Hrvoje Corluka <hrvoje.corluka@volvocars.com>
2021-11-22 13:43:09 +0100

    new folder structure and new distributed CMakeLists

---
+++ apps/input_handler/src/main.cpp
@@ -0,0 +1,28 @@
+#include "ncurses.h"
+#include <iostream>
+#include "wh.hpp"
+#include "chrono"
+
+int main(){
+    WriteHandler wh;
+    initscr();
+    cbreak();
+    noecho();
+    // keypad(stdscr, TRUE);
+    scrollok(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+    while (true) {
+        char in;
+        while ((in = getch()) == ERR) {}
+        if (in >= '0' && in <= '9') {
+            wh.SetThrottle(in-'0');
+        }
+        if (in == 'b') {
+           wh.ToggleBreak();
+        }
+        while (getch() != ERR) {}
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+
+
+}
