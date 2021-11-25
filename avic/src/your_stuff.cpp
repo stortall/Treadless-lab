c73f070a791f93fb987cd85e7143ee73863e5ce3
Prakar Tyagi <prakar.tyagi2@volvocars.com>
2021-11-25 13:58:00 +0100

    correction on hazard lights(not working still,direct icons working)

--- avic/src/your_stuff.cpp
+++ avic/src/your_stuff.cpp
@@ -10,8 +10,6 @@ namespace CANID
     const canid_t TO_IC_FROM_EMU = 0x321;
     const canid_t TO_IC_FROM_INPUT = 0x123;
     const canid_t ICONS =0x333;
-
-    //int toEmuFromInput = 0x123;
 }// namespace CANID
 
 
@@ -48,86 +46,18 @@ void yourStuff::YouHaveJustRecievedACANFrame(const canfd_frame * const _frame) {
              //icons.hazard = _frame->data[0] >> 0 & 1;
              icn.Bits.hazard=0;
          }
-        
-           //p.hazard = 0;
-    //icons.hazard = _frame->data[0] >> 0 & 1;
-    //  icons.right_blinker = _frame->data[0] >> 1 & 1;
-    // icons.left_blinker = _frame->data[0] >> 2 & 1;
-    // icons.engine_check = _frame->data[0] >> 3 & 1;
-    // icons.oil_check = _frame->data[0] >> 4 & 1;
-    // icons.abs = _frame->data[0] >> 5 & 1;
-    // icons.battery = _frame->data[0] >> 6 & 1;
-    // icons.seat_belt = _frame->data[1] >> 7 & 1;
-
-    // icons.doors_open = _frame->data[1] >> 0 & 1;
-    // icons.high_beam = _frame->data[1] >> 1 & 1;
-    // icons.hand_break = _frame->data[1] >> 2 & 1;
-    //icn.Bits.right_blinker =
 
     this->InstrumentCluster.setIcon(&icn);
-        
-        
-        
-            // this->InstrumentCluster.setIcon(&p);
 
-        
-        //SetIcons(_frame->data[0],_frame->data[1]);
-
-
-    //      if(_frame->data[0]==4)
-    // {
-    //     //icons.hazard = _frame->data[0] >> 0 & 1;
-    // }   
-    //this->InstrumentCluster.setIcon(&icons);
-
-        // static bool once = false;
-
-        // if (!once) {
-        //     p.hazard = 1;
-        //     once = true;
-        // } else
-        //    p.hazard = 0;
-        // QString pp = "icon1: " + QString::number(p) + "%\n"
-        //                        "icon2: " + QString::number(p) + "%\n"
-        //                        "icon3:" +QString::number(p)+ "%\n";
-        // this->InstrumentCluster.setTXT(pp);
-        //this->InstrumentCluster.setIcon(&p);
-        //this->InstrumentCluster.setIcon(&p);
 
         break;
     }
-//    case CANID::example:{
-//       const struct CAN::MSG::_userin *d = reinterpret_cast<const struct CAN::MSG::_userin * >((_frame->data));
-//       this->InstrumentCluster.ignite(d->IGNT);
-//    }
-//       break;
-
-    // case CANID::TO_IC_FROM_EMU {
-    //     //const struct CAN::MSG::Gearbx_t::_bits *d = reinterpret_cast<const struct CAN::MSG::Gearbx_t::_bits * >((_frame->data));
-    //     this->InstrumentCluster.setGearPindle_int(frame->data[2]);
-    //     //this->InstrumentCluster.setGear(_frame->data[2]);
-    // }
-    //    break;
-    //case CAN::MSG::ENGINE_ID: {
-    //     const struct CAN::MSG::_engine *d = reinterpret_cast<const struct CAN::MSG::_engine * >((_frame->data));
-    //     //this->InstrumentCluster.ignite(d->RUN);
-    //     this->InstrumentCluster.setRPM(d->RPM);
-    //     this->InstrumentCluster.setSpeed(d->SPD);
-
-    // }
-    //     break;
     default:
         break;
     }
 
 }
 
-void yourStuff::SetIcons(uint8_t data1,uint8_t data2)
-{
-    
-
-
-}
 
 /******************************* ANYTHING BELOW THIS LINE IS JUST BORING STUFF *******************************/
 
