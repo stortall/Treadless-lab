4b512770e535a9ddde028a101daf8fe6340c9252
jinsi.jasi <jinsi.jasi16@gmail.com>
2021-11-26 02:32:40 +0100

    cleaning input main added userinput reader header

--- apps/input_handler/src/main.cpp
+++ apps/input_handler/src/main.cpp
@@ -1,67 +1,16 @@
+#include <userinput_reader.h>
 #include <iostream>
 #include "chrono"
-#include "ncurses.h"
-#include "wh.hpp"
+
+
 
 int main() {
-  WriteHandler wh;
-  initscr();
-  cbreak();
-  noecho();
-  // keypad(stdscr, TRUE);
-  scrollok(stdscr, TRUE);
-  nodelay(stdscr, TRUE);
+  InputReader reader;
+  
+
   while (true) {
-    char in;
-    while ((in = getch()) == ERR) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));}
-    if (in >= '0' && in <= '9') {
-      wh.SetThrottle(in - '0');
-    }
-    if (in == 'b') {
-      wh.ToggleBreak();
-    }
-    if (in == 'p') {
-      wh.SetGearSelectorState('P');
-    }
-    if (in == 'n') {
-      wh.SetGearSelectorState('N');
-    }
-    if (in == 'd') {
-      wh.SetGearSelectorState('D');
-    }
-    if (in == 'h') {
-      wh.ToggleHazard();
-    }
-    if (in == 'o') {
-      wh.ToggleOilCheck();
-    }
-    if (in == 'a') {
-      wh.ToggleABS();
-    }
-    if (in == 'l') {
-      wh.ToggleHighBeam();
-    }
-    if (in == 'z') {
-      wh.ToggleSeatBelt();
-    }
-    if (in == 'c') {
-      wh.ToggleEngineCheck();
-    }
-    if (in == 'y') {
-      wh.ToggleLeftBlinker();
-    }
-    if (in == 'u') {
-      wh.ToggleRightBlinker();
-    }
-    if (in == 'x') {
-      wh.ToggleDoorsOpen();
-    }
-    if (in == 'q') {
-      wh.ToggleBattery();
-    }
-    while (getch() != ERR) {
-    }
+    reader.readInput();
+    
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
 }
